f026a1702e243aae2b7ba852626b9f62ef8923fc
colinwang117 <colinwang117@foxmail.com>
2019-01-03 13:59:47 +0800

    修改

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,2 +1,27 @@
 var allbuttons = $('buttons>span')
 
+$(p1).on('click',function(){
+  $(images).css({
+    transform:'translateX(0)'
+  })
+})
+$(p2).on('click',function(){
+  $(images).css({
+    transform:'translateX(-600px)'
+  })
+})
+$(p3).on('click',function(){
+  $(images).css({
+    transform:'translateX(-1200px)'
+  })
+})
+$(p4).on('click',function(){
+  $(images).css({
+    transform:'translateX(-1800px)'
+  })
+})
+$(p5).on('click',function(){
+  $(images).css({
+    transform:'translateX(-2400px)'
+  })
+})
