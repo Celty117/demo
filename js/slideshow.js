cab61938407e30294bc54f9fc2b4f3a0ec78faea
colinwang117 <colinwang117@foxmail.com>
2019-01-03 22:26:59 +0800

    修改

--- js/slideshow.js
+++ js/slideshow.js
@@ -9,29 +9,37 @@ for (let i = 0; i < allbuttons.length; i++) {
       transform: 'translateX(' + px + 'px)'
     })
     n = index
-    allbuttons.eq(n).addClass('red')
-    .siblings('.red').removeClass('red')
+    activeButton(allbuttons.eq(n))
   })
 }
 
 var n = 0;
 var size = allbuttons.length
-var timerId = setInterval(() => {
-  n += 1
-  allbuttons.eq(n % size).trigger('click')
+palySlide(n % size)
+
+var timerId = setTimer()
+
+function setTimer() {
+  setInterval(() => {
+    n += 1
+    palySlide(n % size)
+  }, 2000)
+}
+
+function palySlide(index) {
+  allbuttons.eq(index).trigger('click')
+}
+
+function activeButton($button) {
+  $button
     .addClass('red')
     .siblings('.red').removeClass('red')
-}, 1000)
+}
 
 $('.slideshow').on('mouseenter', function () {
   window.clearInterval(timerId)
 })
 
 $('.slideshow').on('mouseleave', function () {
-  timerId = setInterval(() => {
-    n += 1
-    allbuttons.eq(n % size).trigger('click')
-      .addClass('red')
-      .siblings('.red').removeClass('red')
-  }, 1000)
+  timerId = setTimer()
 })
