8b90f86382f579846b01e6bdf24e691e29ed9cee
colinwang117 <colinwang117@foxmail.com>
2019-01-03 17:11:02 +0800

    修改

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,11 +1,37 @@
 var allbuttons = $('#buttons>button')
 
+
 for (let i = 0; i < allbuttons.length; i++) {
-  $(allbuttons[i]).on('click', function (x) {
+  $(allbuttons[i]).on('click', function (x) {  // $(allbuttons[i]) === allbuttons.eq(i)
     var index = $(x.currentTarget).index()
-    var n = index * -600
+    var px = index * -600
     $('#images').css({
-      transform: 'translateX(' + n + 'px)'
+      transform: 'translateX(' + px + 'px)'
     })
+    n = index
+    allbuttons.eq(n).addClass('red')
+    .siblings('.red').removeClass('red')
   })
 }
+
+var n = 0;
+var size = allbuttons.length
+var timerId = setInterval(() => {
+  n += 1
+  allbuttons.eq(n % size).trigger('click')
+    .addClass('red')
+    .siblings('.red').removeClass('red')
+}, 1000)
+
+$('.slideshow').on('mouseenter', function () {
+  window.clearInterval(timerId)
+})
+
+$('.slideshow').on('mouseleave', function () {
+  timerId = setInterval(() => {
+    n += 1
+    allbuttons.eq(n % size).trigger('click')
+      .addClass('red')
+      .siblings('.red').removeClass('red')
+  }, 1000)
+})
