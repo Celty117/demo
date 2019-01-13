75b26aaa9856df1cedd6a5ece1c003a828df89b2
colinwang117 <colinwang117@foxmail.com>
2019-01-13 17:40:36 +0800

    新建浮层

---
+++ js/fuceng.js
@@ -0,0 +1,13 @@
+$(clickMe).on('click', function () {
+  if (popover.style.display === 'block') {
+    $(popover).hide()
+  } else {
+    $(popover).show()
+    $(document).one('click', function () {
+      $(popover).hide()
+    })
+  }
+})
+$(wrapper).on('click', function (a) {
+  a.stopPropagation()
+})
